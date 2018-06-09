00b2c6dad7953ca6dcfa98ac6598db1f4789f07a
KnightNiwrem <merwinwee@gmail.com>
2018-06-09 23:26:10 +0800

    Reinitialized repository

---
+++ index.js
@@ -0,0 +1,21 @@
+const { isEmpty, isNil } = require('lodash');
+
+//const RabbitMQService = require('./services/rabbitmq');
+const TelegramService = require('./services/telegram');
+
+const telegramService = new TelegramService();
+const telegramMessageSubject = telegramService.getMessageSubject();
+
+telegramMessageSubject.subscribe((request) => {
+  // Ignore response without text messages
+  const update = request.body;
+  if (isNil(update.message) || isEmpty(update.message.text)) {
+    return;
+  }
+
+  const chat_id = update.message.chat.id;
+  const text = update.message.text;
+  const message = { chat_id, text };
+
+  telegramService.sendMessage({ message });
+});
---
+++ services/telegram/index.js
@@ -0,0 +1,86 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const fetch = require('node-fetch');
+const { Subject } = require('rxjs');
+const config = require('./config');
+
+const { hostname, port, token } = config.getProperties();
+
+class TelegramService {
+  constructor({ 
+    hostname = TelegramService.defaultHostname,
+    port = TelegramService.defaultPort,
+    token = TelegramService.defaultToken
+  } = {}) {
+    this.app = express();
+    this.messageSubject = new Subject();
+
+    this.app.use(bodyParser.json());
+    this.setWebhook();
+    this.subscribeToUpdates();
+    this.connect();
+  }
+
+  connect({
+    port = TelegramService.defaultPort,
+  } = {}) {
+    if (this.server) {
+      this.server.close();
+      console.log('Telegram bot server has shut down');
+    }
+
+    this.server = this.app.listen(port, () => {
+      console.log('Telegram bot server has started');
+    });
+  }
+
+  getMessageSubject() {
+    return this.messageSubject;
+  }
+
+  sendMessage({
+    message = {},
+    token = TelegramService.defaultToken,
+  } = {}) {
+    const request = message;
+    return this._sendRawRequest({ request, token });
+  }
+
+  setWebhook({
+    hostname = TelegramService.defaultHostname,
+    token = TelegramService.defaultToken,
+  } = {}) {
+    const request = {
+      url: `https://${hostname}/${token}`
+    };
+    return this._sendRawRequest({ request, telegramMethod: 'setWebhook', token });
+  }
+
+  subscribeToUpdates({
+    token = TelegramService.defaultToken,
+  } = {}) {
+    this.app.post(`/${token}`, (request, response) => {
+      this.messageSubject.next(request);
+      response.end();
+    });
+  }
+
+  _sendRawRequest({ 
+    headers = { 'Content-Type': 'application/json' }, 
+    httpMethod = 'POST',
+    telegramMethod = 'sendMessage', 
+    request = {},
+    token = TelegramService.defaultToken,
+  } = {}) {
+    const body = JSON.stringify(request);
+    const url = `https://api.telegram.org/bot${token}/${telegramMethod}`;
+
+    return fetch(url, { body, headers, method: httpMethod });
+  }
+}
+
+TelegramService.defaultHostname = hostname;
+TelegramService.defaultPort = port;
+TelegramService.defaultToken = token;
+
+module.exports = TelegramService;
