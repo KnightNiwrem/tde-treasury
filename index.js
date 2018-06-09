738f2d3b459b1389684be4488507b1302d92ba25
KnightNiwrem <merwinwee@gmail.com>
2018-06-10 02:44:46 +0800

    Tighten SQL calls

--- index.js
+++ index.js
@@ -85,8 +85,10 @@ Common: ${commonItemEntry.quantity}`
   const newPersonalQuantity = Math.max(personalItemEntry.quantity + quantity, 0);
   const newPersonalItemEntry = await Item.query()
   .patch({ quantity: newPersonalQuantity })
-  .where('telegramId', telegramId)
+  .where('itemCode', itemCode)
+  .andWhere('telegramId', telegramId)
   .returning('*');
+  const hasIncreasedPersonalQuantity = newPersonalItemEntry.quantity >= personalItemEntry.quantity;
 
   // This suggests a withdraw - check if common pool needs
   // to be updated too
@@ -95,15 +97,17 @@ Common: ${commonItemEntry.quantity}`
   const newCommonQuantity = isWithdrawal ? candidateCommonQuantity : commonItemEntry.quantity;
   const newCommonItemEntry = await Item.query()
   .patch({ quantity: newCommonQuantity })
-  .where('telegramId', commonPoolId)
+  .where('itemCode', itemCode)
+  .andWhere('telegramId', commonPoolId)
   .returning('*');
+  const hasIncreasedCommonQuantity = newCommonItemEntry.quantity >= commonItemEntry.quantity;
 
   const message = {
     chat_id: chatId,
     text: `Processed ${isWithdrawal ? 'withdrawal' : 'deposit'} for ${Math.abs(quantity)} ${itemCode}.
 
-Personal: ${newPersonalItemEntry.quantity} (${newPersonalItemEntry.quantity >= personalItemEntry.quantity ? '+' : '-'}${Math.abs(newPersonalItemEntry.quantity - personalItemEntry.quantity)})
-Common: ${commonItemEntry.quantity} (${newCommonItemEntry.quantity >= commonItemEntry.quantity ? '+' : '-'}${Math.abs(newCommonItemEntry.quantity - commonItemEntry.quantity)})`
+Personal: ${newPersonalItemEntry.quantity} (${hasIncreasedPersonalQuantity ? '+' : '-'}${Math.abs(newPersonalItemEntry.quantity - personalItemEntry.quantity)})
+Common: ${commonItemEntry.quantity} (${hasIncreasedCommonQuantity ? '+' : '-'}${Math.abs(newCommonItemEntry.quantity - commonItemEntry.quantity)})`
   };
   return telegramService.sendMessage({ message });
 };
